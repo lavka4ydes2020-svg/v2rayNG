99e4dde7dcf2f26549650dd0fc6464cdf42ecaf9
Ilnur Habiba <lavka4ydes2020-svg@users.noreply.github.com>
2026-05-28 18:48:06 +0500

    chore: bump version to 2.2.29 (759)

--- V2rayNG/app/build.gradle.kts
+++ V2rayNG/app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.alfredovpn"
         minSdk = 24
         targetSdk = 37
-        versionCode = 758
-        versionName = "2.2.28"
+        versionCode = 759
+        versionName = "2.2.29"
         multiDexEnabled = true
 
         val abiFilterList = (properties["ABI_FILTERS"] as? String)?.split(';')
